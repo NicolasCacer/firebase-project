fd95497a8f9f7ab6cce175248363b77c4c603777
Nicolas <njcaceres04@gmail.com>
2024-12-08 20:49:48 -0500

    Added secrets as .env

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,18 @@
-// Import Firebase SDK modules
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
+import * as dotenv from "dotenv";
 
-// Replace this with your Firebase project's configuration
+dotenv.config();
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCoJoVufH_E3kgcCkOE1bF-hv8zFapT1vs",
-    authDomain: "nicos-project-eea29.firebaseapp.com",
-    projectId: "nicos-project-eea29",
-    storageBucket: "nicos-project-eea29.firebasestorage.app",
-    messagingSenderId: "744859576138",
-    appId: "1:744859576138:web:b1019ba2689d7fc64dc3b3",
-    measurementId: "G-PJRT4L02VD"
-}
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
