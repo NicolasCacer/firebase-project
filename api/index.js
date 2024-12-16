a806b5e535374569554aafb9111c094be2f771a5
Nicolas <njcaceres04@gmail.com>
2024-12-16 18:37:13 -0500

    added users route

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import userRouter from '../routes/users.js';
 
 
 const app = express();
@@ -7,6 +8,8 @@ const port = 3000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/users',userRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Esta ruta no existe' });
--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,14 @@ import { getAllUsers, getUserById } from '../controllers/userControllers.js';
 const userRouter = Router();
 
 // Route to fetch all users
-userRouter.get('/', getAllUsers);
-
-// Route to fetch a user by their ID
-userRouter.get('/:id', getUserById);
+userRouter.get('/', (req, res) => {
+    res.send('List of all users');
+  });
+  
+// Define a route for getting a specific user by ID
+userRouter.get('/:id', (req, res) => {
+    const userId = req.params.id;
+    res.send(`User details for user with ID: ${userId}`);
+  });
 
 export default userRouter;
