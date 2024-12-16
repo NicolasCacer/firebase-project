5943b5c9d63a14da016a691b99992b6bdd12c29d
Nicolas <njcaceres04@gmail.com>
2024-12-16 09:19:14 -0500

    first attempt

--- api/index.js
+++ api/index.js
@@ -1,26 +1,10 @@
-import express from 'express';        // Express.js framework
-import dotenv from 'dotenv';         // dotenv to load environment variables
-import userRouter from '../routes/userRoutes.js';
-
-dotenv.config(); // Load environment variables
-
-// Initialize Express app
+import express from 'express';
+import dotenv from 'dotenv';
 const app = express();
-const port = process.env.PORT || 3000; // Use the port from environment or default to 3000
+dotenv.config();
 
-// Middleware to parse JSON
-app.use(express.json());
-
-// Set routes for the server
-//app.use('/users', userRouter);
-
-// If in a local environment, start the Express server with app.listen
-app.use((req, res) => {
-  res.status(404).json({ message: "Ruta no encontrada" });
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
 });
 
-app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
-});
-
-export default app;
+app.listen(3000, () => console.log('Server ready on port 3000.'));
