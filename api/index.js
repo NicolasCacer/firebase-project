379cbe7da2ee7fb7297b7bf57a0e01970e57f3ed
Nicolas <njcaceres04@gmail.com>
2024-12-16 09:44:03 -0500

    fourth attempt

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-
+import userRouter from '../routes/users';
 
 const app = express();
 const port = 3000;
@@ -8,23 +8,16 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/',(req, res) => {
-  res.send({message:'Hello, world!'});
-});
+app.use('/users', userRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Esta ruta no existe' });
 });
-
 app.use((err, req, res, next) => {
   console.error(err.stack); // Loggear el error para depuración
   res.status(500).json({
     message_error: err.message || 'Error interno del servidor',
   });
-});
-
-app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
 });
 
 export default app;
