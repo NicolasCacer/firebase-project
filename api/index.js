1cc707ef17599549089c9750bdeacc650e2dcb75
Nicolas <njcaceres04@gmail.com>
2024-12-16 18:29:19 -0500

    restart server

--- api/index.js
+++ api/index.js
@@ -1,14 +1,12 @@
 import express from 'express';
 import cors from 'cors';
-import userRouter from '../routes/users';
+
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
-
-app.use('/users', userRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Esta ruta no existe' });
@@ -19,5 +17,8 @@ app.use((err, req, res, next) => {
     message_error: err.message || 'Error interno del servidor',
   });
 });
+app.listen(port,(req,res)=>{
+  console.log(`Server running on http://localhost:${port}`)
+})
 
 export default app;
