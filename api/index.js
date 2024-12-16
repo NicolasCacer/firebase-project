c88edc3edded2bc995a414ec731473e5edb7416d
Nicolas <njcaceres04@gmail.com>
2024-12-16 09:39:19 -0500

    fourth attempt

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import userRouter from '../routes/users.js';
+
 
 const app = express();
 const port = 3000;
@@ -8,7 +8,9 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
-app.use('/users', userRouter);
+app.get('/',(req, res) => {
+  res.send('Hello, world!');
+});
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Esta ruta no existe' });
