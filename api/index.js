a6647657668b94b212a4747714c37015ca8fc31d
Nicolas <njcaceres04@gmail.com>
2024-12-16 09:21:33 -0500

    secodn attempt

--- api/index.js
+++ api/index.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import userRouter from '../routes/userRoutes.js';
+
 const app = express();
 dotenv.config();
 
 app.get('/', (req, res) => {
     res.send('Hello, world!');
 });
+app.use(express.json());
+app.use('/users',userRouter);
+
 
 app.listen(3000, () => console.log('Server ready on port 3000.'));
