9a31aa137cb4401e1785b61ad6e2166114904b1d
Nicolas <njcaceres04@gmail.com>
2024-12-18 23:31:52 -0500

    improved cors

--- src/api/app.js
+++ src/api/app.js
@@ -1,12 +1,25 @@
 import express from "express";
 import userRouter from "../routes/users.js";
 import cors from "cors";
+import "dotenv/config";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const allowedOrigins = ["http://localhost:3000", process.env.FRONT_END_URL];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Origin not allowed by CORS"));
+    }
+  },
+};
+
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use("/users", userRouter);
 app.get("/", (req, res) => {
   res.send("API is running");
