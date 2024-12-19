7ad12f92f5fbd079a591c9d6c25d36e6294fad27
Nicolas <njcaceres04@gmail.com>
2024-12-19 00:30:13 -0500

    added * origin cors

--- src/api/app.js
+++ src/api/app.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use("/users", userRouter);
 app.get("/", (req, res) => {
   res.send("API is running");
