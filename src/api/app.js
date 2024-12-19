ddd1ccb2b13161a30d87563535aa6ac7909aca47
Nicolas <njcaceres04@gmail.com>
2024-12-18 23:21:14 -0500

    Added cors

--- src/api/app.js
+++ src/api/app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import userRouter from "../routes/users.js";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use("/users", userRouter);
 app.get("/", (req, res) => {
   res.send("API is running");
