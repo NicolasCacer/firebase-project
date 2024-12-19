9a9eb86aae0633c55843d17a2f96f05fefa7d1ef
Nicolas <njcaceres04@gmail.com>
2024-12-19 00:00:27 -0500

    All cors accepted

--- src/api/app.js
+++ src/api/app.js
@@ -6,20 +6,8 @@ import "dotenv/config";
 const app = express();
 const port = process.env.PORT || 3000;
 
-const allowedOrigins = ["http://localhost:3000", process.env.FRONT_END_URL];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Origin not allowed by CORS"));
-    }
-  },
-};
-
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 app.use("/users", userRouter);
 app.get("/", (req, res) => {
   res.send("API is running");
