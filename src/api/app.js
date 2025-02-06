8b0e5531c9e12df70c2043ebe9180c3b4a4e523b
Nicolas <njcaceres04@gmail.com>
2025-02-05 21:43:13 -0500

    added corrections to cors

--- src/api/app.js
+++ src/api/app.js
@@ -5,16 +5,17 @@ import "dotenv/config";
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-app.use(express.json());
 app.use(
   cors({
     origin: [
-      "http://localhost:3000/",
-      "https://firebase-project-delta.vercel.app/",
-    ],
+      "http://localhost:3000",
+      "https://next-project-liart-three.vercel.app/",
+    ], // Add your frontend URLs
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
   })
 );
+app.use(express.json());
 app.use("/users", userRouter);
 app.get("/", (req, res) => {
   res.send("API is running");
